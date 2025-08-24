c621bf0c772df19b5c0f91c3a0d8ba1270208e0f
Ruslan Abibullaev <ruslan_abibullaev@mail.ru>
2025-08-25 00:08:26 +0500

    Create computer and human choice functions

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+const CHOICE_OPTIONS = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice(choicesArr) {
+  const randomIndex = Math.floor(Math.random() * choicesArr.length + 1);
+  return choicesArr[randomIndex];
+}
+
+function getHumanChoice() {
+  const humanChoice = prompt('Rock, Paper or Scissors?')
+  return humanChoice;
+}
