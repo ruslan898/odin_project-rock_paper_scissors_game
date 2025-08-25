fd2af68d9c083c1df7626487a116a20ea706cfa3
Ruslan Abibullaev <ruslan_abibullaev@mail.ru>
2025-08-25 12:06:12 +0500

    Add a function for determining the result of the game round

--- script.js
+++ script.js
@@ -11,3 +11,46 @@ function getHumanChoice() {
   const humanChoice = prompt('Rock, Paper or Scissors?');
   return humanChoice;
 }
+
+function playRound(humanChoice, computerChoice) {
+  const humanChoiceLowercase = humanChoice.toLowerCase();
+  if (humanChoiceLowercase === 'rock' && computerChoice === 'paper') {
+    console.log(`You lose! Paper beats Rock`);
+    computerScore++;
+  } else if (
+    humanChoiceLowercase === 'rock' &&
+    computerChoice === 'scissors'
+  ) {
+    console.log(`You win! Rock beats Scissors`);
+    humanScore++;
+  } else if (humanChoiceLowercase === 'paper' && computerChoice === 'rock') {
+    console.log(`You win! Paper beats Rock`);
+    humanScore++;
+  } else if (
+    humanChoiceLowercase === 'paper' &&
+    computerChoice === 'scissors'
+  ) {
+    console.log(`You lose! Scissors beats Paper`);
+    computerScore++;
+  } else if (
+    humanChoiceLowercase === 'paper' &&
+    computerChoice === 'scissors'
+  ) {
+    console.log(`You lose! Scissors beats Paper`);
+    computerScore++;
+  } else if (
+    humanChoiceLowercase === 'scissors' &&
+    computerChoice === 'rock'
+  ) {
+    console.log(`You lose! Rock beats Scissors`);
+    computerScore++;
+  } else if (
+    humanChoiceLowercase === 'scissors' &&
+    computerChoice === 'paper'
+  ) {
+    console.log(`You win! Scissors beats Paper`);
+    humanScore++;
+  } else if (humanChoiceLowercase === computerChoice) {
+    console.log("It's a draw!");
+  }
+}
