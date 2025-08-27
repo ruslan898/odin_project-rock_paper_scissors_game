7e049fa5fd05d94aab580d1dbe65a0e415792a25
Ruslan Abibullaev <ruslan_abibullaev@mail.ru>
2025-08-27 14:08:55 +0500

    Complete game functionality
    Game functionality is complete, but js code is in need of refactoring

--- script.js
+++ script.js
@@ -1,65 +1,110 @@
 const CHOICE_OPTIONS = ['rock', 'paper', 'scissors'];
+let playerScore = 0;
+let computerScore = 0;
+
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+const resultDisplayBox = document.querySelector('.result-display-box');
+const scoreDisplayBox = document.querySelector('.score-display-box');
+const playerScoreDisplayBox = document.querySelector('.player-score span');
+const computerScoreDisplayBox = document.querySelector('.computer-score span');
+const retryBtn = document.querySelector('.retry');
+
+retryBtn.style.display = 'none';
+retryBtn.addEventListener('click', () => {
+  resultDisplayBox.textContent = '';
+  rockBtn.classList.toggle('hide');
+  paperBtn.classList.toggle('hide');
+  scissorsBtn.classList.toggle('hide');
+  playerScore = 0;
+  computerScore = 0;
+  playerScoreDisplayBox.textContent = '0';
+  computerScoreDisplayBox.textContent = '0';
+  retryBtn.style.display = 'none';
+});
+
+rockBtn.addEventListener('click', () => {
+  playRound('Rock', getComputerChoice(CHOICE_OPTIONS));
+});
+paperBtn.addEventListener('click', () => {
+  playRound('Paper', getComputerChoice(CHOICE_OPTIONS));
+});
+scissorsBtn.addEventListener('click', () => {
+  playRound('Scissors', getComputerChoice(CHOICE_OPTIONS));
+});
 
 function getComputerChoice(choicesArr) {
   const randomIndex = Math.floor(Math.random() * choicesArr.length);
   return choicesArr[randomIndex];
 }
 
-function getHumanChoice() {
-  const humanChoice = prompt('Rock, Paper or Scissors?');
-  return humanChoice;
-}
-
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  function playRound(humanChoice, computerChoice) {
-    const humanChoiceLowercase = humanChoice.toLowerCase();
-    if (humanChoiceLowercase === 'rock' && computerChoice === 'paper') {
-      console.log(`You lose! Paper beats Rock`);
-      computerScore++;
-    } else if (
-      humanChoiceLowercase === 'rock' &&
-      computerChoice === 'scissors'
-    ) {
-      console.log(`You win! Rock beats Scissors`);
-      humanScore++;
-    } else if (humanChoiceLowercase === 'paper' && computerChoice === 'rock') {
-      console.log(`You win! Paper beats Rock`);
-      humanScore++;
-    } else if (
-      humanChoiceLowercase === 'paper' &&
-      computerChoice === 'scissors'
-    ) {
-      console.log(`You lose! Scissors beats Paper`);
-      computerScore++;
-    } else if (
-      humanChoiceLowercase === 'paper' &&
-      computerChoice === 'scissors'
-    ) {
-      console.log(`You lose! Scissors beats Paper`);
-      computerScore++;
-    } else if (
-      humanChoiceLowercase === 'scissors' &&
-      computerChoice === 'rock'
-    ) {
-      console.log(`You lose! Rock beats Scissors`);
-      computerScore++;
-    } else if (
-      humanChoiceLowercase === 'scissors' &&
-      computerChoice === 'paper'
-    ) {
-      console.log(`You win! Scissors beats Paper`);
-      humanScore++;
-    } else if (humanChoiceLowercase === computerChoice) {
-      console.log("It's a draw!");
-    }
-  }
-
-  for (let i = 0; i < 5; i++) {
-    playRound(getHumanChoice(), getComputerChoice(CHOICE_OPTIONS));
+function updateScoreDisplay(winner) {
+  if (winner === 'player') {
+    playerScoreDisplayBox.textContent = +playerScoreDisplayBox.textContent + 1;
+  } else {
+    computerScoreDisplayBox.textContent =
+      +computerScoreDisplayBox.textContent + 1;
   }
 }
 
-playGame();
+function playRound(humanChoice, computerChoice) {
+  scoreDisplayBox.classList.remove('hide');
+  const humanChoiceLowercase = humanChoice.toLowerCase();
+  if (humanChoiceLowercase === 'rock' && computerChoice === 'paper') {
+    resultDisplayBox.textContent = `You lose! Paper beats Rock`;
+    computerScore++;
+    updateScoreDisplay('computer');
+  } else if (humanChoiceLowercase === 'rock' && computerChoice === 'scissors') {
+    resultDisplayBox.textContent = `You win! Rock beats Scissors`;
+    playerScore++;
+    updateScoreDisplay('player');
+  } else if (humanChoiceLowercase === 'paper' && computerChoice === 'rock') {
+    resultDisplayBox.textContent = `You win! Paper beats Rock`;
+    playerScore++;
+    updateScoreDisplay('player');
+  } else if (
+    humanChoiceLowercase === 'paper' &&
+    computerChoice === 'scissors'
+  ) {
+    resultDisplayBox.textContent = `You lose! Scissors beats Paper`;
+    computerScore++;
+    updateScoreDisplay('computer');
+  } else if (
+    humanChoiceLowercase === 'paper' &&
+    computerChoice === 'scissors'
+  ) {
+    resultDisplayBox.textContent = `You lose! Scissors beats Paper`;
+    computerScore++;
+    updateScoreDisplay('computer');
+  } else if (humanChoiceLowercase === 'scissors' && computerChoice === 'rock') {
+    resultDisplayBox.textContent = `You lose! Rock beats Scissors`;
+    computerScore++;
+    updateScoreDisplay('computer');
+  } else if (
+    humanChoiceLowercase === 'scissors' &&
+    computerChoice === 'paper'
+  ) {
+    resultDisplayBox.textContent = `You win! Scissors beats Paper`;
+    playerScore++;
+    updateScoreDisplay('player');
+  } else if (humanChoiceLowercase === computerChoice) {
+    resultDisplayBox.textContent = "It's a draw!";
+  }
+
+  if (playerScore === 5) {
+    resultDisplayBox.textContent = 'You have won the game!';
+    rockBtn.classList.toggle('hide');
+    paperBtn.classList.toggle('hide');
+    scissorsBtn.classList.toggle('hide');
+    scoreDisplayBox.classList.toggle('hide');
+    retryBtn.style.display = 'block';
+  } else if (computerScore === 5) {
+    resultDisplayBox.textContent = 'Computer has won the game!';
+    rockBtn.classList.toggle('hide');
+    paperBtn.classList.toggle('hide');
+    scissorsBtn.classList.toggle('hide');
+    scoreDisplayBox.classList.toggle('hide');
+    retryBtn.style.display = 'block';
+  }
+}
